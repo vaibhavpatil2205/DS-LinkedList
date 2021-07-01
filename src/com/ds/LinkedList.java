ea013541ccaaf9debfa7f8f64b35c242d5330692
unknown <vaibhavpatil5471@gmail.com>
2021-07-01 18:45:21 +0100

    Code For Creation Of Linked List

--- src/com/ds/LinkedList.java
+++ src/com/ds/LinkedList.java
@@ -12,9 +12,8 @@ public class Linkedlist {
 			 this.next = null;
 		 }
 	 }
-	 
-	 	// method of inserting data
-	 	public Node insert (int data) {			
+	public void insert (int data) {
+			
 			Node newNode = new Node(data);
 			if (head == null) {
 				head = newNode;
@@ -25,46 +24,15 @@ public class Linkedlist {
 				}
 				temp.next = newNode;
 			}
-			return newNode;
 		}
-	
-		//method to push
-		public void push(int data) {
-			 Node newNode = new Node(data);
-			 newNode.next = head;
-			 head = newNode;
-		 }
 
-		// method to display inserted data
 		public void print() {
 			Node temp = head;
 			while(temp!=null) {
 				System.out.print(temp.data+"->");
 				temp = temp.next;
 			}
-		System.out.println();
+			System.out.println();
 		}
 
-		// method to insert after a node
-		public void insertAfter(Node prevNode, int value) {
-
-			if (prevNode == null) {
-			System.out.println("Prev Node should not be null");
-			return;
-			}
-			
-			Node newNode = new Node(value);
-			newNode.next = prevNode.next;
-			prevNode.next = newNode;
-			}
-
-		public int pop() {
-			int popData = 0;
-			if (head == null) {
-			System.out.println("Stack Over flow");
-			}
-			popData = head.data;
-			head = head.next;
-			return popData;
-			}
 }
--- src/com/ds/Main.java
+++ src/com/ds/Main.java
@@ -2,15 +2,14 @@ package com.ds;
 
 public class Main {
 	public static void main(String[] args) {
-		Linkedlist linkedlist=new Linkedlist();
+		System.out.println("Welcome to DS");
+		
+		
+		Linkedlist linkedlist = new Linkedlist();
 		linkedlist.insert(56);
 		linkedlist.insert(30);
 		linkedlist.insert(70);
-		linkedlist.print();
-
-		int pop = linkedlist.pop();
-		 System.out.println(pop);
 		
-		linkedlist.print();	
+		linkedlist.print();
 		}
 }
