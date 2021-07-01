1ef60dcb8820476e6aa418cdc9783a408d874dbb
unknown <vaibhavpatil5471@gmail.com>
2021-07-01 13:24:02 +0100

    Added

---
+++ src/com/ds/Main.java
@@ -0,0 +1,16 @@
+package com.ds;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		System.out.println("Welcome to data structure");
+		
+		LinkedList linkedList = new LinkedList();
+	    linkedList.insert(56);
+	    linkedList.insert(45);
+	    linkedList.insert(70);
+	    
+	    linkedList.print();
+	    
+	}
+}
