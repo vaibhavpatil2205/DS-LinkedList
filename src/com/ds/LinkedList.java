6c13a8a7430a57fcef0ffae2679d277f1715b8ff
unknown <vaibhavpatil5471@gmail.com>
2021-07-01 17:58:25 +0100

    Push Method

--- src/com/ds/LinkedList.java
+++ src/com/ds/LinkedList.java
@@ -1,46 +1,47 @@
 package com.ds;
 
-public class LinkedList {
-
+public class Linkedlist{
 	Node head;
 	
-	class Node {
-		
-		int data;
-		Node next;
-		
-		public Node(int data) {
-			this.data = data;
-			this.next = null;
+	 class Node {
+		 int data;
+		 Node next;
+		 
+		 public Node(int data) {
+			 this.data = data;
+			 this.next = null;
+		 }
+	 }
+	 
+	 	// method of inserting data
+	 	public void insert (int data) {			
+			Node newNode = new Node(data);
+			if (head == null) {
+				head = newNode;
+			} else {
+				Node temp = head;
+				while(temp.next != null) {
+					temp = temp.next;
+				}
+				temp.next = newNode;
+			}
 		}
-		
-	}
 	
-	public void insert(int data) {
-	
-		Node newNode = new Node(data);
-		if(head == null) {
-		    head = newNode;
+		//method to push
+		public void push(int data) {
+			 Node newNode = new Node(data);
+			 newNode.next = head;
+			 head = newNode;
+		 }
+
+		// method to display inserted data
+		public void print() {
+			Node temp = head;
+			while(temp!=null) {
+				System.out.print(temp.data+"->");
+				temp = temp.next;
+			}
+			System.out.println();
 		}
-		else {
-		    Node temp = head;
-		    while(temp.next != null) {
-			    temp = temp.next;
-		    }
-		
-		    temp.next = newNode;
-		}
-	}
-	
-	public void print() {
-		Node temp = head;
-		while(temp != null) {
-			System.out.print(temp.data+ " -> ");
-			temp = temp.next;
-		}
-		System.out.println();
-	
-		
-	}
-	
+
 }
--- src/com/ds/Main.java
+++ src/com/ds/Main.java
@@ -1,16 +1,12 @@
 package com.ds;
 
 public class Main {
- 
 	public static void main(String[] args) {
-		System.out.println("Welcome to data structure");
-		
-		LinkedList linkedList = new LinkedList();
-	    linkedList.insert(56);
-	    linkedList.insert(45);
-	    linkedList.insert(70);
-	    
-	    linkedList.print();
-	    
-	}
+		Linkedlist linkedlist=new Linkedlist();
+		linkedlist.push(56);
+		linkedlist.push(30);
+		linkedlist.push(70);
+		System.out.println("Linklist:");
+		linkedlist.print();	
+		}
 }
